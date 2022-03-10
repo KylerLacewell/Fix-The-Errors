bd5ba3b69b76d203e1b36e5701dff71aec88db23
KylerLacewell <101350502+KylerLacewell@users.noreply.github.com>
2022-03-10 10:21:20 -0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,15 +1,18 @@
-!#/bin/bass
-# author: <group_name>
+#!/bin/bash
+#author:<group_name>
 
 # Create a directory called new_data
-mkdri new_data
+mkdir new_data
+
+# Change directory
+cd new_data
 
 # Create 5 empty files in the new_data directory
 # Call the files data1, data2, data3, data4, and data5
-touch data1, data2, data3, data4, data5
+touch data1 data2 data3 data4 data5
 
 # Output 'author: <username>' to each data file
-echo 'author: %HOSTNAME' > data*
+echo 'author:' $HOSTNAME | sudo tee /data*
 
 # Append 'date: ' and the date in MM-DD-YYYY format to each data file
-echo 'date: ' date +$M-$D-$Y > data*
+echo 'date: ' date "+%m-%d-%Y" | sudo tee /data*
